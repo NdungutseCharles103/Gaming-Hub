15cb256c45c3b64ae55f98595f08fadb07a51921
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-05-28 19:23:57 +0200

    session

--- CRUDE/signup.php
+++ CRUDE/signup.php
@@ -4,6 +4,7 @@ $firstName=$_POST["firstname"];
 $lastName=$_POST["lastname"];
 $telephone=$_POST["tel"];
 $gender=$_POST["gender"];
+$role=$_POST["role"];
 $nationality=$_POST["Nationality"];
 $username=$_POST["username"];
 $email = $_POST["email"];
@@ -17,34 +18,34 @@ else{
     define('HOST', "localhost");
     $DB_host="localhost";
     $DB_user="root";
-    $DB_password="chazard10.3";
-    $DB_name="db_mis";
+    $DB_password="";
+    $DB_name="users";
     $connection= mysqli_connect($DB_host,$DB_user,$DB_password,$DB_name);
     if (!$connection) {
        echo 'connection error'.mysqli_connect_error();
     }else{
-        // $encrypt = hash('SHA',);
-      /*   $insertQuery="INSERT INTO mis_users(firstName, lastName, gender, telephone,email,nationality, username,password,cpassword) values
-        ('$firstName','$lastName','$gender','$telephone','$email','$nationality','$username','$password','$cpassword')";
+        $encrypt = md5($password);
+        $insertQuery="INSERT INTO mis_users(firstName, lastName, gender, telephone,email,nationality,role, username,password) values
+        ('$firstName','$lastName','$gender','$telephone','$email','$nationality','$role','$username','$encrypt')";
 
-        $insert = mysqli_query($connection,$insertQuery) or die("Error occurred".mysqli_error($connection)); */
-        $sql="SELECT * FROM mis_users";
-        $select = mysqli_query($connection, $sql) or die("Error occurred".mysqli_error($connection));
-        echo "";
-        while ($row = mysqli_fetch_assoc($select)){
-            echo "tr";
-            echo "<td>".$row['user_id']."</td>";
-            echo "<td>".$row['firstname']."</td>";
-            echo "<td>".$row['lastname']."</td>";
-            echo "<td>".$row['gender']."</td>";
-            echo "<td>".$row['telephone']."</td>";
-        }
+        $insert = mysqli_query($connection,$insertQuery) or die("Error occurred".mysqli_error($connection)); 
+        // $sql="SELECT * FROM mis_users";
+        // $select = mysqli_query($connection, $sql) or die("Error occurred".mysqli_error($connection));
+        // echo "";
+        // while ($row = mysqli_fetch_assoc($select)){
+        //     echo "tr";
+        //     echo "<td>".$row['user_id']."</td>";
+        //     echo "<td>".$row['firstname']."</td>";
+        //     echo "<td>".$row['lastname']."</td>";
+        //     echo "<td>".$row['gender']."</td>";
+        //     echo "<td>".$row['telephone']."</td>";
+        // }
 
-       /*  if ($insert) {
+        if ($insert) {
             echo 'insert successfully';
         }else {
             echo 'failed to add a new user';
-        } */
+        }
 
     }
 
---
+++ SESSIONS/login.php
@@ -0,0 +1,35 @@
+<?php
+
+$username = $_GET["username"];
+$password = $_GET["password"];
+
+    define('HOST', "localhost");
+    $DB_host="localhost";
+    $DB_user="root";
+    $DB_password="";
+    $DB_name="users";
+    $connection= mysqli_connect($DB_host,$DB_user,$DB_password,$DB_name);
+
+    if (!$connection) {
+        echo "connection failed";
+    }else{
+        $encrypt = md5($password);
+        $select = mysqli_query($connection, "SELECT username from mis_users where username='$username'
+         && password='$encrypt'")  or die(mysqli_error($connection));
+         if(!$select){
+             echo"unable to login";
+         }else{
+             if (mysqli_num_rows($select)>0) {
+                $name = mysqli_fetch_array($select);
+                // var_dump($name);
+                $user = $name["username"];
+                echo " Hello $user";
+             } else {
+                 echo "No user found";
+             }
+             
+           
+         }
+    }
+
+?>
