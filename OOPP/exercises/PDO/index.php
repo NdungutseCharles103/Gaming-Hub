bbc41ab1b35e6e942c98d5457e94d4378304135e
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-12 21:48:23 +0200

    pdo

--- OOPP/exercises/PDO/index.php
+++ OOPP/exercises/PDO/index.php
@@ -1,24 +1,15 @@
 <?php
 
-    $mysql = new mysqli("localhost", "root", "chazard10.3", "rwanda");
-    $mysql->set_charset("utf8");
+    $pdo= new PDO('mysql:host=localhost;dbname=rwanda', 'root', 'chazard10.3');
+    
+    $sql = "SELECT * FROM villages";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $villages = $stmt->fetchAll();
 
-    $query = $mysql->query("SELECT * from villages LIMIT 3") or die($mysql->error);
-
-    require("../../6.9/FPDF/fpdf.php");
-    $pdf =  new FPDF('P','mm','A4');
-    $pdf->AddPage();
-    $pdf->SetFont('Arial','',12);
-
-    while ($village = $query->fetch_assoc()) {
-        $line=0;
-        $pdf->Cell(40,10,$village["villageName"],1,$line,'C');
-        $pdf->Cell(40,10,$village["cellId"],1,$line,'C');
-        $pdf->Cell(40,10,$village["villageId"],1,$line,'C');
-        $line++;
+    foreach ($villages as $village) {
+        echo $village['villageName'] . '<br>';
     }
+    $pdo = null;
 
-    $pdf->Output();
-    echo "Done";
-    $mysql->close();
-?>
+?>  
